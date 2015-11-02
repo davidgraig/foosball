0b667387d892e2d2ad343926eaf60e427caf35be
davidr <david.russell@hootsuite.com>
2015-11-01 23:55:52 -0800

    add increment and decrement score functions

--- parse/cloud/main.js
+++ parse/cloud/main.js
@@ -6,36 +6,83 @@ Parse.Cloud.define("activateTable", function(request, response) {
 	table.id = tableId;
 
 	// Set a new value on quantity
-	table.set("isInUse", true);
+	table.set("locked", true);
 
 	// Save
 	table.save(null, {
 	  success: function(table) {
-	    response.success("activated");
+	    response.success("locked table");
 	  },
 	  error: function(table, error) {
-	    response.error("Error activating table: " + error);
+	    response.error("Error locking table: " + error);
 	  }
 	});
 });
 
-Parse.Cloud.define("deactivateTable", function(request, response) {
+Parse.Cloud.define("resetTable", function(request, response) {
 	tableId = request.params.tableId;
 
 	var Table = Parse.Object.extend("Table");
 	var table = new Table();
 	table.id = tableId;
 
-	// Set a new value on quantity
-	table.set("isInUse", false);
+	table.set("locked", false);
 
 	// Save
 	table.save(null, {
 	  success: function(table) {
-	    response.success("deactivated");
+	    response.success("unlocked");
 	  },
 	  error: function(table, error) {
-	    response.error("Error activating table: " + error);
+	    response.error("Error unlocking table: " + error);
+	  }
+	});
+});
+
+Parse.Cloud.define("playerScored", function(request, response) {
+	tableId = request.params.tableId;
+	playerNumber = request.params.playerNumber;
+
+	var Table = Parse.Object.extend("Table");
+	var table = new Table();
+	table.id = tableId;
+	if (playerNumber === 1) {
+		table.increment("player1Score");	
+	} else {
+		table.increment("player2Score");
+	}
+	
+	// Save
+	table.save(null, {
+	  success: function(table) {
+	    response.success("score updated");
+	  },
+	  error: function(table, error) {
+	    response.error("Error updating score: " + error);
+	  }
+	});
+});
+
+Parse.Cloud.define("playerGoalDisallowed", function(request, response) {
+	tableId = request.params.tableId;
+	playerNumber = request.params.playerNumber;
+
+	var Table = Parse.Object.extend("Table");
+	var table = new Table();
+	table.id = tableId;
+	if (playerNumber === 1) {
+		table.decrement("player1Score");	
+	} else {
+		table.decrement("player2Score");
+	}
+	
+	// Save
+	table.save(null, {
+	  success: function(table) {
+	    response.success("score updated");
+	  },
+	  error: function(table, error) {
+	    response.error("Error updating score: " + error);
 	  }
 	});
 });
