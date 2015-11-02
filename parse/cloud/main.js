fb4d7ca472308df61053f97c949ddeea87125da2
davidr <david.russell@hootsuite.com>
2015-11-01 20:01:43 -0800

    activate and deactivate cloud functions

--- parse/cloud/main.js
+++ parse/cloud/main.js
@@ -1,6 +1,41 @@
+Parse.Cloud.define("activateTable", function(request, response) {
+	tableId = request.params.tableId;
 
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+	var Table = Parse.Object.extend("Table");
+	var table = new Table();
+	table.id = tableId;
+
+	// Set a new value on quantity
+	table.set("isInUse", true);
+
+	// Save
+	table.save(null, {
+	  success: function(table) {
+	    response.success("activated");
+	  },
+	  error: function(table, error) {
+	    response.error("Error activating table: " + error);
+	  }
+	});
+});
+
+Parse.Cloud.define("deactivateTable", function(request, response) {
+	tableId = request.params.tableId;
+
+	var Table = Parse.Object.extend("Table");
+	var table = new Table();
+	table.id = tableId;
+
+	// Set a new value on quantity
+	table.set("isInUse", false);
+
+	// Save
+	table.save(null, {
+	  success: function(table) {
+	    response.success("deactivated");
+	  },
+	  error: function(table, error) {
+	    response.error("Error activating table: " + error);
+	  }
+	});
 });
